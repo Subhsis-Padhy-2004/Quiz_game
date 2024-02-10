d1010758f57ce8c55d4cde7422f929c9f7779703
Subhasis-Padhy-2004 <subhsispadhy@gmail.com>
2024-02-10 22:35:33 +0530

    Project Initialized

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "what does HTML stand for ?",
+        answer: "C.Hyper Text Markup Language",
+        options: [
+            "A.hyper text multi language",
+            "B.hyper text multiple language",
+            "C.Hyper Text Markup Language",
+            "D.home text multi language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "ଭାରତ ରେ ଶାସନ ମୁଖ୍ୟ କିଏ?",
+        answer: "D.ରାଷ୍ଟ୍ରପତି",
+        options: [
+            "A.ମନ୍ତ୍ରୀ ମଣ୍ଡଳ",
+            "B.ପ୍ରଧାନମନତ୍ରୀ",
+            "C.ବିରୋଧୀ ଦଳ",
+            "D.ରାଷ୍ଟ୍ରପତି"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "ଭାରତ କେତେବର୍ଷ କାଳ ପରାଧୀନ ଥିଲା?",
+        answer: "D.୨୦୦ ବର୍ଷ",
+        options: [
+            "A.୧୫୦ ବର୍ଷ",
+            "B.୨୫୦ ବର୍ଷ",
+            "C.୩୦୦ ବର୍ଷ",
+            "D.୨୦୦ ବର୍ଷ",
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "ଲୋକସଭାର ସଭାପତି କିଏ?",
+        answer: "A.ବାଚସ୍ପତି",
+        options: [
+            "A.ବାଚସ୍ପତି",
+            "B.ଉପରାଷ୍ଟ୍ରପତି",
+            "C.ରାଷ୍ଟ୍ରପତି",
+            "D.ପ୍ରଧାନମନ୍ତ୍ରୀ",
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "ଭାରତ ପାର୍ଲିଆମେଣ୍ଟ ରେ କେତୋଟି ସଭା?",
+        answer: "C.2",
+        options: [
+            "A.4",
+            "B.3",
+            "C.2",
+            "D.1"
+
+        ]
+    }
+];
